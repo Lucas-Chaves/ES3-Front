28d3d67fdc700e25ffc3a582d8f5b692646241af
lucas-chaves <lucas.limachaves@outlook.com>
2019-06-10 22:05:45 -0300

    IMC add

---
+++ src/components/Imc.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id='imc'>
+        <h2>Índice de massa corporal (IMC)</h2>
+        <input type="number" name="peso" v-model="nome" placeholder="Peso Atual">
+        <input type="number" name="altura" v-model="nome" placeholder="Altura Atual">
+        <button type="button" v-on:click="salvarImc">Salvar</button>
+    </div>
+</template>

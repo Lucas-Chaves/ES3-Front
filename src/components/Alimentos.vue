6986bf648cf1f4e0e0e79d57bebbb29defb29855
lucas-chaves <lucas.limachaves@outlook.com>
2019-06-11 00:49:50 -0300

    alimentos

--- src/components/Alimentos.vue
+++ src/components/Alimentos.vue
@@ -2,9 +2,18 @@
     <div id='Grupo'>
         <h2>Cadastro Alimentos</h2>
         <input type="text" name="nAlimento" v-model="name" placeholder="Nome do Alimento">
-        <input type="text" name="proteina" v-model="name" placeholder="Nome do Grupo">
-        <input type="text" name="carboidrato" v-model="name" placeholder="Nome do Grupo">
-        <input type="text" name="vEnergetico" v-model="name" placeholder="Nome do Grupo">
-        <input type="text" name="description" v-model="desc" placeholder="Descrição do Grupo" v-on:keyup.enter="salvarGrupo"><br/>
+        <input type="text" name="proteina" v-model="prot" placeholder="Proteina">
+        <input type="text" name="carboidrato" v-model="carb" placeholder="Carboidrato">
+        <input type="text" name="vEnergetico" v-model="vener" placeholder="Valor Energetico">
+        <div>
+          <select>
+            <option value="1">Tipo 1</option>
+            <option value="2">Tipo 2</option>
+            <option value="3">Tipo 3</option>
+            <option value="4">Tipo 4</option>
+            <option value="5">Tipo 5</option>
+          </select>
+        </div>
         <button type="button" v-on:click="salvarGrupo">Salvar</button>
     </div>
+</template>

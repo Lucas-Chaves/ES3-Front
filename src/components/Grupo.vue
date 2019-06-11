57523cfac10e4aa4bda8cacce8bd02c967079638
ArchanjoJr <archanjojr@my.com>
2019-06-10 23:49:10 -0300

    add another crud

--- src/components/Grupo.vue
+++ src/components/Grupo.vue
@@ -1,8 +1,40 @@
 <template>
     <div id='Grupo'>
         <h2>Grupo de Alimentos</h2>
-        <input type="text" name="nome" v-model="nome" placeholder="Nome do Grupo">
-        <input type="text" name="description" v-model="nome" placeholder="Descrição do Grupo"><br/>
-        <button type="button" v-on:click="salvarImc">Salvar</button>
+        <input type="text" name="nome" v-model="name" placeholder="Nome do Grupo">
+        <input type="text" name="description" v-model="desc" placeholder="Descrição do Grupo" v-on:keyup.enter="salvarGrupo"><br/>
+        <button type="button" v-on:click="salvarGrupo">Salvar</button>
     </div>
 </template>
+<script>
+  import axios from 'axios';
+  import { mapState } from 'vuex'
+  export default {
+    name:'grupo',
+    computed:mapState(['token']),
+    data() {
+        return {
+          name: '',
+          desc: ''
+        }
+    },
+    methods: {
+      salvarGrupo: function() {
+        console.log({ name: this.name, desc: this.desc })
+        axios.post('/grupo', {
+          headers: {
+            Authorization: `Bearer ${this.token}`
+          },
+          nome: this.name,
+          description: this.desc
+        })
+        .then(resp => {
+          if(resp.status === 201 || resp.status === 200 ) alert(' grupo criado com sucesso !');
+        })
+        .catch(error => {
+          alert(error.response)
+        });
+      }
+    }
+    }
+</script>

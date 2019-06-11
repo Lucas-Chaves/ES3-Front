fa6c4369ac25a4d55e89a3a91dc00d1ee72221f5
lucas-chaves <lucas.limachaves@outlook.com>
2019-06-10 22:20:49 -0300

    Imc pronto

--- src/components/Grupo.vue
+++ src/components/Grupo.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <h2>GRupo</h2>
-  </div>
+    <div id='imc'>
+        <h2>Índice de massa corporal (IMC)</h2>
+        <input type="number" name="peso" v-model="nome" placeholder="Peso Atual">
+        <input type="number" name="altura" v-model="nome" placeholder="Altura Atual"><br/>
+        <button type="button" v-on:click="salvarImc">Salvar</button>
+    </div>
 </template>
--- src/components/Imc.vue
+++ src/components/Imc.vue
@@ -2,7 +2,17 @@
     <div id='imc'>
         <h2>Índice de massa corporal (IMC)</h2>
         <input type="number" name="peso" v-model="nome" placeholder="Peso Atual">
-        <input type="number" name="altura" v-model="nome" placeholder="Altura Atual">
+        <input type="number" name="altura" v-model="nome" placeholder="Altura Atual"><br/>
         <button type="button" v-on:click="salvarImc">Salvar</button>
     </div>
 </template>
+
+
+
+<style>
+    input[type=number]::-webkit-inner-spin-button, 
+    input[type=number]::-webkit-outer-spin-button { 
+    -webkit-appearance: none; 
+    margin: 0; 
+    }
+</style>
